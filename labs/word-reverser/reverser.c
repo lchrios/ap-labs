aac75f889498412007d5771349c484f3d8f63a8a
Christopher I Ortega <a01634500@itesm.mx>
2020-09-01 23:46:00 +0000

    temp
    Signed-off-by: Christopher I Ortega <a01634500@itesm.mx>

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main(){
     // Place your magic here
+    char c, str[100];
+    int i = 0;
+
+    while(true){
+
+        i = 0;
+                
+        printf("Enter a word to reverse: ");
+
+        c = getchar();
+        while (c != "\n") {
+
+        }
+
+        printf("Introduced word: %c\n", c);
+    }
+    
+
     return 0;
 }
