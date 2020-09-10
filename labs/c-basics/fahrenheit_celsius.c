94aaf588d3e028cdf4c3dfba7bd19010c1199e5e
Christopher Ortega <a01634500@itesm.mx>
2020-09-10 01:56:55 +0000

    Solve c-basics
    Signed-off-by: Christopher Ortega <a01634500@itesm.mx>

--- labs/c-basics/fahrenheit_celsius.c
+++ labs/c-basics/fahrenheit_celsius.c
@@ -8,15 +8,33 @@
 
 int main(int argc, char **argv)
 {
-    int fahr;
+    // valor de conversion
     int LOWER;
-    sscanf(argv[1],"%d", &LOWER);
-    int UPPER;
-    sscanf(argv[2],"%d", &UPPER);
-    int STEP;
-    sscanf(argv[3],"%d", &STEP);
-    for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
-    printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+    // conversion simple
+    if (argc == 2) {
+
+        sscanf(argv[1], "%d", &LOWER);
+        printf("Fahrenheit: %3d, Celcius: %4f1\n", LOWER, (5.0/9.0)*(LOWER-32));
+
+    } 
+    // conversion con rango
+    else if (argc == 4) {
+
+        int UPPER, STEP, fahr;
+        sscanf(argv[1], "%d", &LOWER);
+        sscanf(argv[2],"%d", &UPPER);
+        sscanf(argv[3],"%d", &STEP);
+        for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+            printf("Fahrenheit: %3d, Celcius: %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
+
+    } 
+    // argumentos inválidos
+    else {
+
+        printf("Argumentos introducidos inválidos.\n");
+
+    }
 
     return 0;
 }
