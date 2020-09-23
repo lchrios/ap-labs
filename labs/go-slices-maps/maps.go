7053771607d962d01b3ae55233ee67a9da52fa96
Christopher Ortega <a01634500@itesm.mx>
2020-09-23 00:58:25 +0000

    Solve go-slices-maps
    Signed-off-by: Christopher Ortega <a01634500@itesm.mx>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+// WordCount ...
+func WordCount(s string) map[string]int {
+
+	daWords := strings.Split(s, " ")
+
+	cntWords := make(map[string]int)
+
+	for i := 0; i < len(daWords); i++ {
+		cntWords[daWords[i]]++
+	}
+
+	return cntWords
+
+}
+
 func main() {
+
+	wc.Test(WordCount)
+
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -2,10 +2,24 @@ package main
 
 import "golang.org/x/tour/pic"
 
+// Pic ...
 func Pic(dx, dy int) [][]uint8 {
-     return [][]uint8{}
+
+	daPicture := make([][]uint8, dy)
+
+	for i := 0; i < dy; i++ {
+		daPicture[i] = make([]uint8, dx)
+		for j := 0; j < dx; j++ {
+			daPicture[i][j] = uint8(i ^ j)
+		}
+	}
+
+	return daPicture
+
 }
 
 func main() {
+
 	pic.Show(Pic)
+
 }
