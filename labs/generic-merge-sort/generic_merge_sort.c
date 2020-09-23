d78af022b8ae0108665c1ccd563595150ce2cf3f
Christopher Ortega <a01634500@itesm.mx>
2020-09-23 01:34:29 +0000

    Solve generic-merge-sort
    Signed-off-by: Christopher Ortega <a01634500@itesm.mx>

--- labs/generic-merge-sort/generic_merge_sort.c
+++ labs/generic-merge-sort/generic_merge_sort.c
@@ -1,230 +1,156 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-//#include "intop.h"
-//#include "strop.h"
+#include <stdlib.h>
 
-void CopyArray(void *array1, void *array2, int arrLen, int index, 
-		void Assign(void*, void*, int i, int j))
+#define MAX_LINES 10000
+
+void mergeSort(void *array[], int l, int r, int (*compare)(void *, void *));
+void merge(void *array[], int l, int m, int r, int (*compare)(void *, void *));
+int intcmp(char *, char *);
+
+char *lines[MAX_LINES];
+
+int main(int argc, char** argv) 
 {
-	int i;
-	printf("Copying array...\n\t");
-	for (i = 0; i < arrLen; i++)
-		Assign(array1, array2, i, index+i);
-	printf("Copying completed.\n");
-}
+	// default set for string
+    int type = 0;
 
-void Merge(void *array, void* bottom_half, void* top_half, int arrLen,
-	  int Compare(void*, void*, int, int), void Assign(void*, void*, int, int))
-{
-	int i, bot_i = 0, top_i = 0;
-	
-	printf("Merging arrays...\n");
-	for (i = 0; i < arrLen; i++) // for each cell in the result array
-	{
-		// compare lesser values in arrays
-		if (top_i == (arrLen/2 + arrLen%2) || ((bot_i < arrLen/2) && 
-			Compare(bottom_half, top_half, bot_i, top_i))) {
-			// if bot_i is smaller
-			printf("%s > %s | bot_i smaller\n", ((char *)top_half)[top_i], ((char *)bottom_half)[bot_i]);
-			Assign(array, bottom_half, i, bot_i);
-			bot_i++; // next item in bottom_half
-		}
-		else {
-			// if top_i is smaller
-			printf("%s > %s | top_i smaller\n", ((char *)bottom_half)[bot_i], ((char *)top_half)[top_i]);
-			Assign(array, top_half, i, top_i);
-			top_i++; // next item in top_half
-		}
-		
+    if(argc < 2) {
+        printf("Error: wrong input of parameters.\n");
+        printf("Usage: ./generic_merge_sort <<type -n|default for strings>> <<fptrname>>\n");
+        return 1;
+    }
+
+    if(argc == 2 && strcmp(argv[1],"-n") == 0) {
+        printf("Error: wrong set of parameters.\n");
+        printf("Usage: ./generic_merge_sort <<type -n|default is string>> <<fptrname>>\n");
+        return 1;
+    }
+
+	// -n -> flag for numbers manage
+    if(argc == 3 && strcmp(argv[1],"-n") == 0) {
+        type = 1;
+    }
+
+    FILE *fptr;
+
+	if((fptr = fopen(argv[type ? 2 : 1], "r")) == NULL) {
+		printf("Error while opening fptr: %s", argv[1]);
+		return 1;
 	}
-}
+    
+    char line[256] = {0};
+    int i = 0;
+    
+	while(fgets(line, 256, fptr)) {
+        lines[i] = (char*)malloc(strlen(line) + sizeof(char*));
+        strcpy(lines[i],line);
+        i++;
+    }
 
-void Mergesort(void* array, int arrLen, int Size(void),
-		int Compare(void*, void*, int, int), 
-		void Assign(void*, void*, int, int))
-{
-	void* bottom_half; // of the array
-	void* top_half; // of the array
+    if(fclose(fptr))
+        printf("Error while closing the fptr...\n");
+
+
+    mergeSort((void *) lines, 0, i-1, (int (*)(void*, void*)) (type ? intcmp : strcmp));
 	
-	// array is only item
-	if (arrLen == 1)
-		printf("Only IT: %s", ((char *) array)[0]);
-		return;
-	
-	// allocate memory for bottom half and top half
-	bottom_half = malloc(arrLen/2 * Size());
-	top_half = malloc((arrLen/2 + arrLen%2) * Size());
-	
-	// check memory allocations
-	if (!bottom_half || !top_half) {
-		printf("Could not allocate memory");
-		exit(EXIT_FAILURE); // exit
-	}
-	
-	// copy arrays
-	printf("Copying first half:\n\t");
-	CopyArray(bottom_half, array, arrLen/2, 0, Assign);
-	printf("Copying last half:\n\t");
-	CopyArray(top_half, array, (arrLen/2 + arrLen%2), arrLen/2, Assign);
-	
-	// sort each half recursively
-	printf("Merge sorting first half...\n");
-	Mergesort(bottom_half, arrLen/2, Size, Compare, Assign);
-	printf("Merge sorting later half...\n");
-	Mergesort(top_half, (arrLen/2 + arrLen%2), Size, Compare, Assign);
-	
-	// merge them
-	printf("Merging both halves...\n");
-	Merge(array, bottom_half, top_half, arrLen, Compare, Assign);
-		
-	// free allocated memory
-	free(bottom_half);
-	free(top_half);
-}
+	FILE *resf;
 
-int getFileLineLen(FILE *fileptr){
-	int cnt = 0;
-	char chr = getc(fileptr);
-
-	while (chr != EOF) {
-		if (chr == '\n') {
-			cnt++;
-		}
-		chr = getc(fileptr);
-	}
-	fclose(fileptr);
-	return cnt;
-}
-
-// compare a cell in 1 array to a cell in another array
-int StrCompare(void* array1, void* array2, int i, int j) {
-    return strcmp(((const char *) array1 + i), ((const char *) array2 + i));
-}
-// assign value from one array to another
-void StrAssign(void* array1, void* array2, int i, int j) {
-	((char *)array1)[i] = ((char *)array2)[j];
-}
-// returns sizeof(char * 64)
-int StrSize(void) {
-	return sizeof(char) * 64;
-}
-int IntCompare(void* array1, void* array2, int i, int j) {
-	return (((int*)array1)[i] <= ((int*)array2)[j]);
-}
-// assign value from one array to another
-void IntAssign(void* array1, void* array2, int i, int j) {
-	((int *)array1)[i] = ((int *)array2)[j];
-}
-// returns sizeof(int)
-int IntSize(void) {
-	return sizeof(int);
-}
-
-void printArray(char *array[], int len) {
-	for (int i = 0; i < len; i++) {
-		printf("%s\n", array + i);
-	}
-}
-
-void printIntArray(int *array, int len) {
-	for (int i = 0; i < len; i++) {
-		printf("%d\n", array[i]);
-	}
-}
-
-int main(int argc, char *argv[])
-{	
-	char *location;
-	char *line = NULL;
-	ssize_t read;
-	// Merge numbers
-	if (strcmp(argv[1], "-n") == 0 && argv[2] != NULL) {
-		printf("Merging numbers...\n");
-		location = argv[2];
-		size_t len = 256;
-		 
-
-		// read file
-		FILE* fptr = fopen(argv[2], "r");
-
-		int array[len];
-
-		if (fptr == NULL)
-			exit(EXIT_FAILURE);
-
-		// parse file into new array
-		int i = 0;
-		int nLines = 0;
-
-		while (read = getline(&line, &len, fptr) != -1){
-			//printf("Line  %d readed\n", i);
-			array[i] = atoi(line);
-			//printf("line %d | len: %d | value: %d\n", i, read, array[i]);
-			i++;
-		}
-
-		//printIntArray(array, i);
-
-		// merge array
-		printf("Beginning the merge...\n");
-		Mergesort(array, sizeof(array), IntSize, IntCompare, IntAssign);
-
-		//printIntArray(array, i);
-		
+	if (type) {
 		// write in document
-		FILE *resf = fopen("mergedNum.txt", "w+");
-		if (resf == NULL)
-			exit(EXIT_FAILURE);
-
-		for (int i = 0; i < sizeof(array); i++){
-			fprintf(resf, "%d\n", atoi((const char *)array + i));
-		}
-
-		fclose(resf);
-
-	}
-	// Merge strings 
-	else if (argv[1] != NULL && strncmp(argv[1], "-n", 2) != 0) {
-		printf("Merging numbers...\n");
-
-		location = argv[1];
-		 
-
-		// read file
-		FILE* fptr = fopen(location, "r");
-		int len = getFileLineLen(fptr);
-
-		char *array[len];
-
-		printArray(array, len);
-
-		if (fptr == NULL)
-			exit(EXIT_FAILURE);
-
-		// parse file into new array
-		int i = 0;
-		while (fgets(line, sizeof(line), fptr) != NULL){
-			array[i] = line;
-			i++;
-		}
-
-		// merge array
-		Mergesort(array, sizeof(array), StrSize, StrCompare, StrAssign);
+		resf= fopen("mergedStr.txt", "w+");
 		
-		// write in document
-		FILE *resf = fopen("mergedStr.txt", "w+");
-
-		for (int i = 0; i < sizeof(array); i++){
-			fprintf(resf, "%s\n", ((char *)array)[i]);
+		for (int j = 0; j < i; j++){
+			printf("%s", lines[j]);
+			fprintf(resf, "%s", lines[j]);
 		}
+	} else {
+		// write in document
+		resf = fopen("mergedNum.txt", "w+");
+		
+		for (int j = 0; j < i; j++){
+			printf("%s", lines[j]);
+			fprintf(resf, "%s", lines[j]);
+		}
+	}
 
-		fclose(resf);
-	}
-	// Wrong input
-	else {
-		printf("No valid parameters");
-	}
+	fclose(resf);
 
     return 0;
+}
+
+void mergeSort(void* array[], int l, int r, int (*compare)(void *, void *)) {
+    if (l < r) { 
+        int m = l+(r-l)/2; 
+
+        mergeSort(array, l, m, compare); 
+        mergeSort(array, m+1, r, compare); 
+
+        merge(array, l, m, r, compare); 
+    } 
+
+}
+
+void merge(void* array[], int l, int m, int r, int (*compare)(void *, void *)){
+
+    int i, j, k, 
+		top_i = m-l + 1, 
+		bot_i = r - m; 
+
+    void *top[top_i], *bot[bot_i]; 
+
+    for (i = 0; i < top_i; i++)
+        top[i] = array[l + i];
+
+    for (j = 0; j < bot_i; j++) 
+        bot[j] = array[m + 1+ j]; 
+
+    i = 0; 
+    j = 0; 
+    k = l; 
+
+    while (i < top_i && j < bot_i) { 
+
+        if (compare(top[i], bot[j])<=0) { 
+          
+		    array[k] = top[i]; 
+            i++; 
+        
+		} else { 
+        
+		    array[k] = bot[j]; 
+            j++; 
+        
+		} 
+        k++; 
+    } 
+    while (i < top_i) {
+
+        array[k] = top[i]; 
+        i++; 
+        k++; 
+
+    } 
+    while (j < bot_i) {
+
+        array[k] = bot[j]; 
+        j++; 
+        k++;
+
+    }
+}
+
+
+int intcmp(char *s1, char *s2) {
+
+    double v1 = atof(s1), 
+		   v2 = atof(s2);
+
+    if (v1 < v2)
+        return -1;
+    else if(v1 > v2)
+        return 1;
+    else
+        return 0;
+
 }
