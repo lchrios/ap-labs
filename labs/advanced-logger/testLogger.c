b4ff671cac1053a8b7d23f33c0b368de05c261d3
Christopher Ortega <a01634500@itesm.mx>
2020-09-29 04:41:15 +0000

    Solve advanced-logger
    Signed-off-by: Christopher Ortega <a01634500@itesm.mx>

--- labs/advanced-logger/testLogger.c
+++ labs/advanced-logger/testLogger.c
@@ -3,32 +3,47 @@
 #include "logger.h"
 
 int main(int argc, char **argv){
+	int cs;
 
-    printf("%d\n", atoi(argv[1]));
-    switch(atoi(argv[1])) {
-    case 1:
-	// default logging
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    case 2:
-	// stdout logging
-	initLogger("stdout");
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    case 3:
-	// syslog logging
-	initLogger("syslog");
-	infof("INFO Message %d", 1);
-	warnf("WARN Message %d", 2);
-	errorf("ERROR Message %d", 2);
-	break;
-    default:
-	errorf("Invalid test case");
-	break;
+
+	if(argc > 1){
+		cs = atoi(argv[1]);
+		//printf("%d\n", cs);
+	} 
+	// invalid input of data
+	else {
+		cs = -1;
+		//printf("%d\n", cs);
+	}
+
+    switch(cs) {
+		case 1:
+			// default logging
+			infof("INFO Message %d", 1);
+			warnf("WARN Message %d", 2);
+			errorf("ERROR Message %d", 2);
+			break;
+
+		case 2:
+			// stdout logging
+			initLogger("stdout");
+			infof("INFO Message %d", 1);
+			warnf("WARN Message %d", 2);
+			errorf("ERROR Message %d", 2);
+			break;
+		
+		case 3:
+			// syslog logging
+			initLogger("syslog");
+			infof("INFO Message %d", 1);
+			warnf("WARN Message %d", 2);
+			errorf("ERROR Message %d", 2);
+			break;
+		
+		default:
+			initLogger("stdout");
+			errorf("Invalid test case");
+			break;
     }
 
     return 0;
