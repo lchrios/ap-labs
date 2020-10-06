be5617da91b958dd17484936c1ee88f0c18fe993
Christopher <chrortegita@gmail.com>
2020-10-06 17:41:58 -0600

    Solve file-dir-monitor

--- labs/file-dir-monitor/logger.c
+++ labs/file-dir-monitor/logger.c
@@ -1,23 +1,234 @@
 #include <stdio.h>
+#include <string.h>
+#include <syslog.h>
+#include <stdarg.h>
+#include <stdbool.h>
 #include "logger.h"
 
+#define RESET		0
+#define BRIGHT 		1
+#define DIM		    2
+#define UNDERLINE 	3
+#define BLINK		4
+#define REVERSE		7
+#define HIDDEN		8
+
+#define BLACK 		10
+#define RED		    1
+#define GREEN		2
+#define YELLOW		3
+#define BLUE		4
+#define MAGENTA		5
+#define CYAN		6
+#define	WHITE		7
+
+/*
+    LOG_CON | Write directly to system console if there is an error while sending to system logger.
+    LOG_NDELAY | Open the connection immediately 
+*/
+
+void textcolor(int attr, int fg, int bg);
+
+// default - syslog 
+bool isSyslog = true;
+
 int initLogger(char *logType) {
-    printf("Initializing Logger on: %s\n", logType);
+
+    // stdout selected
+    if (strcmp(logType, "") == 0 || strcmp(logType, "stdout") == 0) { 
+        
+        isSyslog = false; 
+        
+    }
+
+    // syslog selected
+    else if (strcmp(logType, "syslog") == 0){ 
+        
+        setlogmask (LOG_UPTO (LOG_DEBUG));
+        isSyslog = true; 
+        
+    }
+
+    // wrong parameter typed
+    else {
+
+        perror("Error: at logging stream selection\nPlease use one of these options: (stdout/syslog)");
+        return 1;
+    
+    }
+
+    // logTpye is valid
+    printf("Logger initialized correctly...\n\tStreaming messages through: %s\n\n", isSyslog ? "syslog" : "stdout");
+
     return 0;
+
 }
+
 
 int infof(const char *format, ...) {
+    
+    // getting parameters 
+    va_list ap;
+    va_start(ap, format);
+
+    // change console style
+    textcolor(BRIGHT, CYAN, BLACK);
+    
+    // Logging data
+    if(isSyslog){
+
+        openlog("INFO", LOG_CONS | LOG_NDELAY, LOG_LOCAL0);
+        vsyslog(LOG_MAKEPRI(LOG_USER, LOG_INFO), format, ap);
+		closelog();
+
+    } else {
+
+        vfprintf(stdout, format, ap);
+    
+    }
+
+    va_end(ap);
+
+    if (isSyslog == 0) {
+
+        // clean stdout console
+		fprintf(stdout, "\n");
+		fflush(stdout);
+	
+    }
+
+    // reset log to normal style
+    textcolor(RESET, WHITE, BLACK);
+
     return 0;
+
 }
+
 
 int warnf(const char *format, ...) {
+
+    // getting parameters
+    va_list ap;
+    va_start(ap,format);
+
+    // change console stlye
+    textcolor(BRIGHT, YELLOW, BLACK);
+
+    // Logging data
+    if (isSyslog){
+
+        //openlog("WARN", LOG_CONS | LOG_NDELAY, LOG_LOCAL0);
+		vsyslog(LOG_MAKEPRI(LOG_USER, LOG_WARNING), format, ap);
+		//closelog();
+    
+    } else {
+       
+        vfprintf(stdout, format, ap);
+	
+    }
+
+    va_end(ap);
+
+    if (isSyslog == 0) {
+        
+		// clean stdout console
+        fprintf(stdout, "\n");
+		fflush(stdout);
+	
+    }
+
+    // reset log to normal style
+    textcolor(RESET, WHITE, BLACK);
+
     return 0;
+
 }
+
 
 int errorf(const char *format, ...) {
+
+    // getting parameters
+    va_list ap;
+    va_start(ap,format);
+
+    // change console stlye
+    textcolor(BRIGHT, RED, BLACK);
+
+    // Logging data
+    if (isSyslog){
+
+        openlog("ERROR", LOG_CONS | LOG_NDELAY, LOG_LOCAL0);
+		vsyslog(LOG_MAKEPRI(LOG_USER, LOG_ERR), format, ap);
+		closelog();
+    
+    } else {
+
+        vfprintf(stdout, format, ap);
+	
+    }
+
+    va_end(ap);
+
+    if (isSyslog == 0) {
+
+	    // clean stdout console	
+        fprintf(stdout, "\n");
+		fflush(stdout);
+	
+    }
+
+    // reset log to normal style
+    textcolor(RESET, WHITE, BLACK);
+
     return 0;
+
 }
+
+
 int panicf(const char *format, ...) {
+    
+    // getting parameters
+    va_list ap;
+    va_start(ap,format);
+
+    // change console stlye
+    textcolor(BRIGHT, MAGENTA, BLACK);
+
+    // Logging data
+    if (isSyslog){
+
+        openlog("PANIC", LOG_CONS | LOG_NDELAY, LOG_LOCAL0);
+		vsyslog(LOG_MAKEPRI(LOG_USER, LOG_EMERG), format, ap);
+		closelog();
+    
+    } else {
+
+        vfprintf(stdout, format, ap);
+
+	}
+    
+    va_end(ap);
+
+    if (isSyslog == 0) {
+
+        // clean stdout console
+		fprintf(stdout, "\n");
+		fflush(stdout);
+	
+    }
+
+    // reset log to normal style
+    textcolor(RESET, WHITE, BLACK);
+    
     return 0;
 }
 
+
+void textcolor(int attr, int fg, int bg) {
+
+    char command[13];
+
+	sprintf(command, "%c[%d;%d;%dm", 0x1B, attr, fg + 30, bg + 40);
+	printf("%s", command);
+
+}
--- labs/file-dir-monitor/monitor.c
+++ labs/file-dir-monitor/monitor.c
@@ -1,7 +1,116 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "logger.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <ftw.h>
+#include <sys/inotify.h>
+#include <string.h>
 
-int main(){
-    // Place your magic here
+#define MAXDIR 200
+#define MAXLEN 1000
+#define BUFFSIZE (10 *(sizeof(struct inotify_event) + MAXLEN + 1))
+
+int inotifyState, op;
+
+struct op {
+    char name[MAXLEN];
+    int op;
+};
+
+struct op op_arr[(10 * (sizeof(struct op) + MAXLEN + 1))];
+int op_index = 0;
+int op_size = 0;
+
+void createop_arr(const char * name, int watchDescriptor){
+    struct op op_struct;
+    strcpy(op_struct.name, name);
+    op_struct.op = op;
+    op_arr[op_index++] = op_struct;
+    op_size++;
+}
+
+static int add_watcher(const char *path, const struct stat *sb, int type){
+    if(type != FTW_D){
+        return 0;
+    }
+
+    int watchDescriptor;
+    if((watchDescriptor = inotify_add_watch(inotifyState, path, IN_CREATE | IN_DELETE | IN_MODIFY)) == -1){
+        errorf("Error: failed creating watch descriptor.");
+        return -1;
+    }
+    createoptruct(path, watchDescriptor);
+    return 0;
+}
+
+char * findWatchDescriptor(int op) {
+    int i;
+    for (i = 0; i < op_size; i++) {
+        if (op_arr[i].op == op) {
+            return op_arr[i].name;
+        }
+    }
+
+    return NULL;
+}
+
+static void printEvent(struct inotify_event *i) {
+    infof("op: %d detected event: %s in file %s", i->op, 
+        i->mask & IN_CREATE ? "IN CREATE" : i->mask & IN_DELETE ? "IN DELETE" :
+        i->mask & IN_MODIFY ? "IN MODIFY" : "???", i->name);   
+}
+
+int main(int argc, char** argv){
+    if(argc < 2){
+        errorf("Error: try using ./monitor <path>");
+        exit(1);
+    }
+
+    const char *path;
+    char buf[BUFFSIZE];
+    ssize_t nrd;
+    char *p;
+
+
+    struct inotify_event *event;
+
+    // Init inotify
+    infof("Initiating inotify");
+    path = argv[1];
+    inotifyState = inotify_init();
+
+
+    if(inotifyState == -1){
+        errorf("Error: failure at the inotify initialization");
+        exit(1);
+    } else {    
+        infof("Successfully initiated inotify");
+    }
+
+    if(ftw(path, add_watcher, MAXDIR) == -1){
+        errorf("Error: failed at traversing the file tree.");
+        exit(1);
+    }
+
+    for(;;){
+        if((nrd = read(inotifyState, buf, BUFFSIZE)) == 0){
+            errorf("Error: failed reading inotify file descriptor.");
+            exit(1);
+        }
+
+        if(nrd == -1){
+            errorf("Error: failed reading inotify file descriptor.");
+            exit(1);
+        }
+
+        for(p = buf; p < buf + nrd;){
+            event = (struct inotify_event * ) p;
+            printEvent(event);
+            p += sizeof(struct inotify_event) + event->len;
+        }
+    }
+
     return 0;
 }
