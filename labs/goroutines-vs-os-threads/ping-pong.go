969c821c087c6c0a45c4aed6fb5d9aa52a338827
Christopher I Ortega <a01634500@itesm.mx>
2020-11-11 01:48:18 +0000

    Solved goroutines-vs-os-threads
    Signed-off-by: Christopher I Ortega <a01634500@itesm.mx>

--- labs/goroutines-vs-os-threads/ping-pong.go
+++ labs/goroutines-vs-os-threads/ping-pong.go
@@ -2,10 +2,38 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	var commsPerSecond int
+	// channels
+	in := make(chan int)
+	out := make(chan int)
+	commsTotal := 0 
+	cps := make([]int, 10) 
 
-	fmt.Println("Communications Per Second : ", commsPerSecond)
+	// linkning channels bothways
+	go link(in, out)
+	go link(out, in)
+
+	// waits for one second periods 5 times
+	for i := 0; i < 5; i++ {
+		out <- 0
+		time.Sleep(time.Duration(1) * time.Second)
+		x := <-in
+		cps[i] = x
+		commsTotal += x
+
+	}
+
+	fmt.Println("Communications Per Second : ", commsTotal, "/", 5, "=",commsTotal/5)
+	fmt.Println("Each second: ", cps)
+}
+
+
+// constant communication between channels
+func link(in chan int, out chan int) {
+	for {
+		out <- (1 + <-in)
+	}
 }
--- labs/goroutines-vs-os-threads/pipeline.go
+++ labs/goroutines-vs-os-threads/pipeline.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+var maxStages int
+
 func main() {
-	var maxStages, transitTime int
+
+	// communication channels	
+	in := make(chan int)
+	out := make(chan int)
+
+	go link(in, out, 0, 1000000) // begin the first connection
+
+	start := time.Now() // start the timer
+
+	in <- 1 // first message
 
 	fmt.Println("Maximum number of pipeline stages   : ", maxStages)
-	fmt.Println("Time to transit trough the pipeline : ", transitTime)
+	fmt.Println("Time to transit trough the pipeline : ", time.Since(start))
+}
+
+func link(in <-chan int, out chan int, i int, conns int) {
+	if i <= conns {
+		maxStages++
+		link(in, out, i + 1, conns)
+		out <- <-in
+	}
 }
